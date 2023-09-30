dcb49d931436824c1279b32c3c23ca6f0e9d4a9c
rtarun9 <rtarun0913@gmail.com>
2023-09-30 20:10:45 +0530

    Added basic osmdroid integration to show current user location on map

--- app/src/main/java/com/example/mad/MainActivity.kt
+++ app/src/main/java/com/example/mad/MainActivity.kt
@@ -1,5 +1,11 @@
 package com.example.mad
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Build
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
@@ -12,7 +18,14 @@ import android.view.Menu
 import android.view.MenuItem
 import android.widget.Button
 import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import org.osmdroid.config.Configuration
 import com.example.mad.databinding.ActivityMainBinding
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,11 +34,74 @@ class MainActivity : AppCompatActivity() {
     private lateinit var leftButton: Button
     private lateinit var rightButton: Button
 
+    private lateinit var locationManager: LocationManager
+
+    private lateinit var currentLocation: Location
+
+    private lateinit var mapView: MapView
+    private lateinit var myLocationOverlay: MyLocationNewOverlay
+
+    private val PERMISSION_REQUEST_CODE = 123
+
+    private val locationListener = object : LocationListener {
+        override fun onLocationChanged(location: Location) {
+            currentLocation = location
+            mapView.controller.setCenter(GeoPoint(currentLocation.latitude, currentLocation.longitude))
+        }
+
+        override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {}
+    }
+
     @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // Request location permissions
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ),
+                PERMISSION_REQUEST_CODE
+            )
+        }
+
+        val ctx = applicationContext
+        Configuration.getInstance().load(ctx, getSharedPreferences("osmdroid", 0))
+
+        mapView = findViewById<MapView>(R.id.mapView)
+        mapView.controller.setZoom(15.0)
+
+        myLocationOverlay = MyLocationNewOverlay(GpsMyLocationProvider(this), mapView)
+        myLocationOverlay.enableMyLocation()
+
+        mapView.overlays.add(myLocationOverlay)
+
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        try {
+            locationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                0,
+                0f,
+                locationListener
+            )
+        } catch (e: SecurityException) {
+            e.printStackTrace()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
 
         vibrationMotor = VibrationMotor(this)
 

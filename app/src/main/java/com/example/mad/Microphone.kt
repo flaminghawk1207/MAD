85964722911b46f2f97ef441e25a9642987b3303
rtarun9 <rtarun0913@gmail.com>
2023-10-25 13:32:51 +0530

    Integrated mapbox geocoding API to Microphone module

--- app/src/main/java/com/example/mad/Microphone.kt
+++ app/src/main/java/com/example/mad/Microphone.kt
@@ -8,6 +8,10 @@ import android.speech.SpeechRecognizer
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import java.util.Locale
 
 // Class that can be used to get user input (via speech) to text format.
@@ -41,6 +45,38 @@ class Microphone : Activity() {
             i.putExtra(RecognizerIntent.EXTRA_PROMPT, "Please tell your destination location")
             startActivityForResult(i, SPEECH_RECORD_REQUEST_CODE, null)
         }
+
+        // NOTE : Only for testing purposes of geocoding API in the emulator where microphone access is not present.
+        // Remove from here once testing is done.
+        // Setup request queue for volley (reference code : https://google.github.io/volley/simple.html).
+        var queue = Volley.newRequestQueue(this)
+
+        val url = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + "Brookfields Mall coimbatore" + ".json?proximity=ip&access_token=pk.eyJ1IjoidGFydW5yIiwiYSI6ImNsbjZ4NzR6cjBsejMyaWp0b2g4bnc4eHQifQ.laPj-90SZU3jpT_1bdGPcA";
+        // Request a string response from the provided URL.
+        val jsonRequest = JsonObjectRequest(Request.Method.GET, url, null,
+            Response.Listener { response ->
+                val featureCollection = response.getJSONArray("features")
+                val placeNames = ArrayList<String>()
+                for (i in 0 until featureCollection.length()) {
+                    val feature = featureCollection.getJSONObject(i)
+                    val geometry = feature.getJSONObject("geometry")
+                    val coordinates = geometry.getJSONArray("coordinates")
+                    val latitude = coordinates.getDouble(1)
+                    val longitude = coordinates.getDouble(0)
+                    val coordinateString = "Latitude: $latitude, Longitude: $longitude"
+                    val placeName = feature.getString("place_name") + coordinateString;
+                    placeNames.add(placeName)
+                }
+
+                val placeNamesText = placeNames.joinToString("\n")
+                textView.text = placeNamesText
+            },
+            Response.ErrorListener { error ->
+                // Handle error
+            }
+        )
+
+        queue.add(jsonRequest)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -57,6 +93,42 @@ class Microphone : Activity() {
             }
 
             textView.text = text;
+        }
+
+
+        // Once text has been updated (if not null), send a request to the mapbox geocoding api.
+        if (textView.text != "") {
+            // Setup request queue for volley (reference code : https://google.github.io/volley/simple.html).
+            var queue = Volley.newRequestQueue(this)
+
+            val url = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + textView.text.toString() + ".json?proximity=ip&access_token=pk.eyJ1IjoidGFydW5yIiwiYSI6ImNsbjZ4NzR6cjBsejMyaWp0b2g4bnc4eHQifQ.laPj-90SZU3jpT_1bdGPcA";
+            // Request a string response from the provided URL.
+            val jsonRequest = JsonObjectRequest(Request.Method.GET, url, null,
+                Response.Listener { response ->
+                    val featureCollection = response.getJSONArray("features")
+                    val placeNames = ArrayList<String>()
+                    for (i in 0 until featureCollection.length()) {
+                        val feature = featureCollection.getJSONObject(i)
+                        val geometry = feature.getJSONObject("geometry")
+                        val coordinates = geometry.getJSONArray("coordinates")
+                        val latitude = coordinates.getDouble(1)
+                        val longitude = coordinates.getDouble(0)
+                        val coordinateString = "Latitude: $latitude, Longitude: $longitude"
+                        val placeName = feature.getString("place_name") + coordinateString;
+                        placeNames.add(placeName)
+                    }
+
+                    val placeNamesText = placeNames.joinToString("\n")
+                    textView.text = placeNamesText
+                },
+                Response.ErrorListener { error ->
+                    // Handle error
+                }
+            )
+
+            queue.add(jsonRequest)
+
+
         }
     }
 }
